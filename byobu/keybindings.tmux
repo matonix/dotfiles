801a80d745afdb92086bb5517d257738ac7275f9
matonix <automatonix@gmail.com>
2018-10-23 00:26:23 +0900

    fix shift-key bindings for micro

--- byobu/keybindings.tmux
+++ byobu/keybindings.tmux
@@ -1,3 +1,7 @@
 set -g prefix F12
 unbind-key -n C-a
 bind-key -n C-S-F6 kill-pane
+unbind-key -n S-Up
+unbind-key -n S-Down
+unbind-key -n S-Left
+unbind-key -n S-Right
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,12 +5,41 @@ set fish_greeting
 set DEFAULT_USER $USER
 
 set PATH $HOME/.local/bin $PATH
+set PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set PATH $HOME/go/bin $PATH
+set PATH /home/linuxbrew/.linuxbrew/opt/go/libexec/bin $PATH
+
+# for fisher v3
+# details: https://github.com/jorgebucaran/fisher/issues/448
+
+if functions -q fish_user_key_bindings
+    if not functions -q _keybinder_fish_user_key_bindings
+        functions -c fish_user_key_bindings _keybinder_fish_user_key_bindings
+    end
+end
+
+test -z "$XDG_CONFIG_HOME"; and set XDG_CONFIG_HOME ~/.config
+
+function fish_user_key_bindings
+    for file in $XDG_CONFIG_HOME/fish/conf.d/*_key_bindings.fish
+        source $file
+    end
+    if functions -q _keybinder_fish_user_key_bindings
+        _keybinder_fish_user_key_bindings
+    end
+end
+
+# end
 
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 alias l "ls"
+alias o "xdg-open"
 alias ghci "stack ghci --"
 alias ghc "stack ghc --"
 alias runghc "stack runghc --"
 
-set EDITOR vi
+set EDITOR micro
+
+alias configfish "$EDITOR $HOME/.config/fish/config.fish"
+alias sourcefish "source $HOME/.config/fish/config.fish"
